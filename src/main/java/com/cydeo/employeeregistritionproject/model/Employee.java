dd51b41fd34fea6f4663eb426a3afa3e6f8cc23c
UmitUmit <umit.green@gmail.com>
2022-03-22 22:43:12 -0400

    employee registration project

---
+++ src/main/java/com/cydeo/employeeregistritionproject/model/Employee.java
@@ -0,0 +1,41 @@
+package com.cydeo.employeeregistritionproject.model;
+
+
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Employee {
+
+//    @NotNull        // except null, anything can be accepted
+//    @NotEmpty       //Except null and empty String anything can be accepted
+//    @NotBlank       // Except null, empty String and only space anything can be accepted.
+
+    @NotBlank
+    @Size(max = 12, min = 2)
+    private String firstName;
+    private String lastName;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")//after adding this kinda pattern thymeleaf then able to understand it
+    private LocalDate birthday;
+    private String email;
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+
+
+
+}
---
+++ src/test/java/com/cydeo/employeeregistritionproject/EmployeeRegistritionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.employeeregistritionproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRegistritionProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
